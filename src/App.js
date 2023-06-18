6a463821d1ed671dd2090ec2371e1b00b5a9f51e
Yasiru Dilshan <121401369+yasirudilshan@users.noreply.github.com>
2023-06-19 02:32:55 +0530

    ChangeImage

--- src/App.js
+++ src/App.js
@@ -10,15 +10,19 @@ function App() {
   function backgroundChange() {
     var temp = weatherData.main.temp - 273;
     console.log(temp);
+ 
 
     if (temp > -10 && temp <= 0) {
-      document.body.style.backgroundImage = "url('https://img.freepik.com/free-photo/3d-winter-snowy-landscape_1048-9671.jpg?w=900&t=st=1686848303~exp=1686848903~hmac=7bf80bf6745ad0fb26d6687b2b0a0976368e0eb3e1a3e1ffb29d6808a30a1d50')";
+      document.body.style.backgroundImage = "url('https://i.ibb.co/zFHcT6G/winter1.jpg')";
+
     } else if (temp > 1 && temp <= 15) {
-      document.body.style.backgroundImage = "url('https://img.freepik.com/free-vector/snow-winter-background-sky-landscape-with-cold-cloud-blizzard-stylized-blurred-snowflakes-snowdrift-realistic-style_333792-43.jpg?w=1380&t=st=1686848432~exp=1686849032~hmac=b434f3ce0426b8925d0fc0a39d97fef25f3a2a2447beb9bd742a4e5b93f2070e')";
+
+      document.body.style.backgroundImage = "url('https://i.ibb.co/x1R4T3t/winter.jpg')";
+
     } else if (temp > 16 && temp <= 25) {
-      document.body.style.backgroundImage = "url('https://img.freepik.com/free-vector/yellow-orange-sunset-background_1308-20105.jpg?w=996&t=st=1686848531~exp=1686849131~hmac=f2038d806870303f29c12d2b187755b9702830a57aefa4f6deb61bbb0100810c')";
+      document.body.style.backgroundImage = "url('https://i.ibb.co/9hRg9Ng/spring-jp.jpg')";
     } else if (temp > 26) {
-      document.body.style.backgroundImage = "url('https://img.freepik.com/free-vector/gradient-summer-heat-background_23-2149423259.jpg?w=996&t=st=1686848530~exp=1686849130~hmac=a2f50449cb15eaff2237969e7742aeb5adb5b564ae69df9661654e778b6002ca')";
+      document.body.style.backgroundImage = "url('https://i.ibb.co/93jgch7/warmth.jpg')";
     }
   }
 
